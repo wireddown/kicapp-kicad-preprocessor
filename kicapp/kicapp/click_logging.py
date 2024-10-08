94728012f6568e7535779a2965dbd3f9c1b82597
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-07 22:25:39 -0700

    Format exceptions

--- kicapp/kicapp/click_logging.py
+++ kicapp/kicapp/click_logging.py
@@ -45,19 +45,26 @@ class ColorFormatter(logging.Formatter):
         self.level = self._checkLevel(level)
 
     def format(self, record):
-        if not record.exc_info:
+        if record.exc_info:
+            default_formatter = logging.Formatter()
+            formatted_message = default_formatter.format(record)
+        else:
             formatted_message = record.getMessage()
-            level = record.levelname.lower()
-            color_style = self.colors.get(level, dict(fg="bright_white"))
-            time_string = ""
-            location_string = ""
-            if self.level < logging.WARNING:
-                timestamp = logging.time.localtime(record.created)
-                time_string = click.style(f"{logging.time.strftime('%Y.%m.%d %H:%M:%S', timestamp)}.{record.msecs:03.0f}", **color_style)
-                location_string = click.style(f"{record.name:>30}::{record.funcName} L{record.lineno:>4}", **color_style)
-            severity_string = click.style(f"{record.levelname:<8}", **color_style)
-            message_strings = [click.style(line, fg="bright_white") for line in formatted_message.splitlines()]
-            entry_prefix = f"{time_string} {location_string} {severity_string}"
-            formatted_entry = '\n'.join(f"{entry_prefix} {line}".strip() for line in message_strings)
-            return formatted_entry
-        return logging.Formatter.format(self, record)
+
+        level = record.levelname.lower()
+        color_style = self.colors.get(level, dict(fg="bright_white"))
+
+        time_string = ""
+        location_string = ""
+        if self.level < logging.WARNING:
+            timestamp = logging.time.localtime(record.created)
+            time_string = click.style(f"{logging.time.strftime('%Y.%m.%d %H:%M:%S', timestamp)}.{record.msecs:03.0f}", **color_style)
+            location_string = click.style(f"{record.name:>30}::{record.funcName} L{record.lineno:>4}", **color_style)
+
+        severity_string = click.style(f"{record.levelname:<8}", **color_style)
+        message_strings = [click.style(line, fg="bright_white") for line in formatted_message.splitlines()]
+
+        entry_prefix = f"{time_string} {location_string} {severity_string}"
+        formatted_entry = '\n'.join(f"{entry_prefix} {line}".strip() for line in message_strings)
+
+        return formatted_entry
