99d4ab5a1ee68c23a76d33543aef2809160810e7
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-08 20:21:05 -0700

    Add tests for get_logging_level()

--- kicapp/tests/test_console.py
+++ kicapp/tests/test_console.py
@@ -1,5 +1,7 @@
 """Tests for the kicapp command line interface."""
 
+import logging
+
 from click.testing import CliRunner
 
 from kicapp import console
@@ -11,3 +13,27 @@ def test_console() -> None:
     result = runner.invoke(console.cli)
     assert result.exit_code == 0
     assert "Show this message and exit." in result.output
+
+
+def test_default_verbosity() -> None:
+    """Does it use WARNING as the default verbosity?"""
+    log_level = console.get_logging_level(quiet=False, verbose=False)
+    assert log_level == logging.WARNING, "Default logging level should be 'logging.WARNING' but it isn't"
+
+
+def test_quiet_verbosity() -> None:
+    """Does it use ERROR as the verbosity for --quiet?"""
+    log_level = console.get_logging_level(quiet=True, verbose=False)
+    assert log_level == logging.ERROR, "Quiet logging level should be 'logging.ERROR' but it isn't"
+
+
+def test_verbose_verbosity() -> None:
+    """Does it use INFO as the verbosity for --verbose?"""
+    log_level = console.get_logging_level(quiet=False, verbose=True)
+    assert log_level == logging.INFO, "Verbose logging level should be 'logging.INFO' but it isn't"
+
+
+def test_quiet_velocity_wins() -> None:
+    """Does --quiet win over --verbose?"""
+    log_level = console.get_logging_level(quiet=True, verbose=True)
+    assert log_level == logging.ERROR, "--quiet should override --verbose but it didn't"
