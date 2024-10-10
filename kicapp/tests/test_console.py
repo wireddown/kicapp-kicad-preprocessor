1a4bd554a9fd8b4e9977e0cba83d14025aea420b
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-09 22:47:09 -0700

    Replace verbosity tests with a truth table

--- kicapp/tests/test_console.py
+++ kicapp/tests/test_console.py
@@ -2,6 +2,7 @@
 
 import logging
 
+import pytest
 from click.testing import CliRunner
 
 from kicapp import console
@@ -15,25 +16,16 @@ def test_console():  # noqa: ANN201
     assert "Show this message and exit." in result.output
 
 
-def test_default_verbosity():  # noqa: ANN201
-    """Does it use INFO as the default verbosity?"""
-    log_level = console.get_logging_level(quiet=False, verbose=False)
-    assert log_level == logging.INFO, "Default logging level should be 'logging.INFO' but it isn't"
-
-
-def test_quiet_verbosity():  # noqa: ANN201
-    """Does it use ERROR as the verbosity for --quiet?"""
-    log_level = console.get_logging_level(quiet=True, verbose=False)
-    assert log_level == logging.ERROR, "Quiet logging level should be 'logging.ERROR' but it isn't"
-
-
-def test_verbose_verbosity():  # noqa: ANN201
-    """Does it use DEBUG as the verbosity for --verbose?"""
-    log_level = console.get_logging_level(quiet=False, verbose=True)
-    assert log_level == logging.DEBUG, "Verbose logging level should be 'logging.DEBUG' but it isn't"
-
-
-def test_quiet_velocity_wins():  # noqa: ANN201
-    """Does --quiet win over --verbose?"""
-    log_level = console.get_logging_level(quiet=True, verbose=True)
-    assert log_level == logging.ERROR, "--quiet should override --verbose but it didn't"
+@pytest.mark.parametrize(
+    ("quiet", "verbose", "expected_log_level", "assert_message"),
+    [
+        (False, False, logging.INFO, "Default logging level should be 'logging.INFO'"),
+        (True, False, logging.ERROR, "Quiet logging level should be 'logging.ERROR'"),
+        (False, True, logging.DEBUG, "Verbose logging level should be 'logging.DEBUG'"),
+        (True, True, logging.ERROR, "--quiet should override --verbose"),
+    ],
+)
+def test_verbosity_truth_table(quiet, verbose, expected_log_level, assert_message):  # noqa: ANN001 ANN201
+    """Validate all combinations of --quiet and --verbose."""
+    log_level = console.get_logging_level(quiet, verbose)
+    assert log_level == expected_log_level, assert_message
