5d8ef8d36e82c6be73c924e2d101f7096c77dd7e
Down to the Wire <8404598+wireddown@users.noreply.github.com>
2024-10-12 13:26:42 -0700

    Fix skipped subcommands and add regression test

--- kicapp/kicapp/console.py
+++ kicapp/kicapp/console.py
@@ -15,7 +15,8 @@ logger = logging.getLogger(__name__)
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Enable verbose messages.")
 @click.version_option()
 @click.option("--logging-demo", is_flag=True, default=False, hidden=True)
-def cli(quiet: bool, verbose: bool, logging_demo: bool) -> None:  # noqa: FBT001 Booleans are in the user interface
+@click.pass_context
+def cli(ctx: click.Context, quiet: bool, verbose: bool, logging_demo: bool) -> None:  # noqa: FBT001 Booleans are in the user interface
     """Run a command to prepare EDA files for importing into KiCad."""
     log_level = get_logging_level(quiet, verbose)
 
@@ -23,6 +24,8 @@ def cli(quiet: bool, verbose: bool, logging_demo: bool) -> None:  # noqa: FBT001
 
     if logging_demo:
         kicapp.click_logging.show_logging_demo(logger)
+    elif ctx.invoked_subcommand:
+        pass
     else:
         cli(["--help"])
 
